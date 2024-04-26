88ae53a7304c2ff83a5e2545023008096ef370e3
Jozef <Jozefw@sbcglobal.net>
2024-04-26 11:01:27 -0700

    add lifted state for game turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import {useState} from "react";
 
 import Players from "./components/players.jsx";
 import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 
 function App() {
-    const [activePlayer,setActivePlayer] = useState('X');
+    const [activePlayer,
+        setActivePlayer] = useState('X');
+    const [playerTurns,
+        SetPlayerTurns] = useState('')
 
-    function handlePlayerRotation(){
-        setActivePlayer((CurrentPlayer)=>{
-            CurrentPlayer === "X" ? "O" : "X"
+    function handlePlayerRotation(rowIndex, columnIndex) {
+        setActivePlayer((CurrentPlayer) => CurrentPlayer === "X"
+            ? "O"
+            : "X";
+        SetPlayerTurns(previousTurns => {
+            const updatedTurns = [
+                {
+                    square: {
+                        row: rowIndex,
+                        col: columnIndex
+                    },
+                    player: activePlayer
+                },
+                ...previousTurns
+            ];
+
         });
     }
 
@@ -16,12 +33,18 @@ function App() {
         <main>
             <div id="game-container">
                 <ol id="players" className="highlight-player">
-                    <Players initialName="Player 1" symbol="X" isActivePlayer={activePlayer === "X"}></Players>
-                    <Players initialName="Player 2" symbol="0" isActivePlayer={activePlayer === "O"}></Players>
+                    <Players
+                        initialName="Player 1"
+                        symbol="X"
+                        isActivePlayer={activePlayer === "X"}></Players>
+                    <Players
+                        initialName="Player 2"
+                        symbol="0"
+                        isActivePlayer={activePlayer === "O"}></Players>
                 </ol>
                 <GameBoard playerChange={handlePlayerRotation} activePlayerIcon={activePlayer}/>
             </div>
-            LOG
+            <Log></Log>
         </main>
     )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,21 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 
 const InitialGame = [
     [null,null,null],
     [null,null,null],
     [null,null,null],
 ]
-export default function GameBoard({playerChange,activePlayerIcon}){
-    const [GameBoard,setGameBoard] = useState(InitialGame)
+export default function GameBoard({playerChange}){
+    // const [GameBoard,setGameBoard] = useState(InitialGame)
 
-    function handleBoardCLick(RowIndex,ColIndex){
-        setGameBoard((previousBoard)=>{
-            const updatedBoard = [...previousBoard.map(innerArrays => [...innerArrays])];
-            updatedBoard[RowIndex][ColIndex] = activePlayerIcon;
-            return updatedBoard;
-        })
-        playerChange();
-    }
+    // function handleBoardCLick(RowIndex,ColIndex){
+    //     setGameBoard((previousBoard)=>{
+    //         const updatedBoard = [...previousBoard.map(innerArrays => [...innerArrays])];
+    //         updatedBoard[RowIndex][ColIndex] = activePlayerIcon;
+    //         return updatedBoard;
+    //     })
+    //     playerChange();
+    // }
     return(
         <ol id="game-board">
             {GameBoard.map((row,RowIndex)=>(
@@ -23,7 +23,8 @@ export default function GameBoard({playerChange,activePlayerIcon}){
             <ol>
                 {row.map((PlayerIcon,ColIndex)=>(
                 <li key={ColIndex}>
-                    <button onClick={()=>{handleBoardCLick(RowIndex,ColIndex)}}>{PlayerIcon}</button>
+                    {/* <button onClick={()=>{handleBoardCLick(RowIndex,ColIndex)}}>{PlayerIcon}</button> */}
+                    <button onClick={playerChange}>{PlayerIcon}</button>
                 </li>
                 ))}
             </ol>
